0d363290f5ed060e287871a4650278ac79cea140
Mariana <marianaishikawamota@gmail.com>
2023-11-17 12:48:06 -0300

    ImersaoDev

---
+++ conversorInterestelar/script.js
@@ -0,0 +1,8 @@
+var nomeUsuario = "Mariana";
+var valorEmAnosLuz = 400;
+var anosLuzEmKm = 9460800000000;
+var anosLuzEmMetros = 9460800000000000;
+var valorEmMetros = (valorEmAnosLuz * anosLuzEmMetros).toFixed(2);
+var valorEmKm = (valorEmAnosLuz * anosLuzEmKm).toFixed(2);
+// valorEmReal = valorEmReal.toFixed(2);
+alert('Olá ' + nomeUsuario + '! ' + valorEmAnosLuz + ' anos-luz correspondem a ' + valorEmMetros + ' metros, o que corresponde a ' + valorEmKm + ' Km');
---
+++ conversormoedas/script.js
@@ -0,0 +1,13 @@
+var valorEmDolar = 64;
+var cotacaoDoDolar = 5.32;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+var valorEmEuro = 64;
+var cotacaoDoEuro = 5.31;
+
+var valorEmRealEuro = valorEmEuro * cotacaoDoEuro;
+valorEmEuro = valorEmEuro.toFixed(2);
+
+alert("R$" + valorEmReal + "\nR$" + valorEmRealEuro); 
